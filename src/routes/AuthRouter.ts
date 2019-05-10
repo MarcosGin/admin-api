c817e773a378995f7a81d387351875e8f60285ce
Marcos Gin <marcosgin291@gmail.com>
2019-05-10 14:16:43 -0300

    Implement logout and login

--- src/routes/AuthRouter.ts
+++ src/routes/AuthRouter.ts
@@ -1,18 +1,30 @@
 import * as express from "express";
 import { BaseRouter } from "./BaseRouter";
+import { Oauth2 } from "../auth/oauth2";
+import { Auth } from "../auth/auth";
+import { AuthManager } from "../managers/AuthManager";
 
 export class AuthRouter extends BaseRouter {
   private authManager: any;
 
   constructor() {
     super();
-    //this.authManager = new AuthManager(); TODO
+    this.authManager = new AuthManager();
     this.buildRoutes();
   }
 
+  public async logout(req: express.Request, res: express.Response, next: express.NextFunction) {
+    try {
+      const user = await this.authManager.logout(req.user);
+      res.json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+
   private buildRoutes() {
-    this.router.post("/token", (req: express.Request, res: express.Response, next: express.NextFunction) => {
-      res.json({ status: true });
-    });
+    const oauth = new Oauth2();
+    this.router.post("/token", oauth.getTokenEndpoint());
+    this.router.post("/logout", Auth.getBearerMiddleware(), this.logout.bind(this));
   }
 }
