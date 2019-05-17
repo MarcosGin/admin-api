e3ec48dcfc1ab94d5d5e754968be4a2fba7e0fee
Marcos Gin <marcosgin291@gmail.com>
2019-05-17 10:09:15 -0300

    Fix bug, incorrect status code

--- src/errors/ErrorHandler.ts
+++ src/errors/ErrorHandler.ts
@@ -8,7 +8,7 @@ export function errorHandler(err, req, res, next) {
     return res.status(401).json(err);
   }
   if (err.name === "AuthenticationError") {
-    return res.status(403).json(err);
+    return res.status(err.status).json(err);
   }
   if (err instanceof NotFoundError) {
     return res.status(404).json(err);
