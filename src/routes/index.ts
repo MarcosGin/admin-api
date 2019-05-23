fc38eb14caff2e358348912c64fbcbdd7ec9d7be
Marcos Gin <marcosgin291@gmail.com>
2019-05-23 10:00:37 -0300

    catch 404 not found routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,13 +4,18 @@ import { UserRouter } from "./UserRouter";
 import { ProductRouter } from "./ProductRouter";
 import { CategoryRouter } from "./CategoryRouter";
 import { BrandRouter } from "./BrandRouter";
+import { NotFoundError } from "../errors/NotFoundError";
 
 export class Router {
   public static initializeRoutes(app: express.Express) {
-    app.use("/oauth", new AuthRouter().router);
-    app.use("/users", new UserRouter().router);
-    app.use("/products", new ProductRouter().router);
-    app.use("/categories", new CategoryRouter().router);
-    app.use("/brands", new BrandRouter().router);
+    app.use("/api/auth", new AuthRouter().router);
+    app.use("/api/users", new UserRouter().router);
+    app.use("/api/products", new ProductRouter().router);
+    app.use("/api/categories", new CategoryRouter().router);
+    app.use("/api/brands", new BrandRouter().router);
+
+    app.use("*", (req: express.Request, res: express.Response, next: express.NextFunction) =>
+      next(new NotFoundError("Not found this endpoint"))
+    );
   }
 }
